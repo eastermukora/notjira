05cdeb05034eef3d5e6352dd0f6d4c2185abcb06
TheMostHigh <tallestlegacy@gmail.com>
2025-07-14 12:28:12 +0300

    test: initial workspaces route

---
+++ routes/workspaces.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::middleware('auth')->group(function () {
+    // list all workspaces
+    Route::get("/workspaces", function () {
+        return "todo";
+    });
+
+    // create a new workspace
+    Route::post("/workspaces", function () {
+        return "todo";
+    });
+
+    // show a workspace
+    Route::get("/workspaces/{workspace_id}", function () {
+        return "todo";
+    });
+
+    // update a workspace
+    Route::put("/workspaces/{workspace_id}", function () {
+        return "todo";
+    });
+
+    // delete a workspace
+    Route::delete("/workspaces/{workspace_id}", function () {
+        return "todo";
+    });
+
+    // create a workspace invite
+    Route::post("/workspaces/{workspace_id}/invites", function () {
+        return "todo";
+    });
+
+    // show workspace invites
+    Route::get("/workspaces/{workspace_id}/invites", function () {
+        return "todo";
+    });
+
+    // create a ticket
+    Route::post("/workspaces/{workspace_id}/tickets", function () {
+        return "todo";
+    });
+
+    // show tickets
+    Route::get("/workspaces/{workspace_id}/tickets", function () {
+        return "todo";
+    });
+
+    // show a ticket
+    Route::get("/workspaces/{workspace_id}/tickets/{ticket_id}", function () {
+        return "todo";
+    });
+
+    // update a ticket
+    Route::put("/workspaces/{workspace_id}/tickets/{ticket_id}", function () {
+        return "todo";
+    });
+
+    // delete a ticket
+    Route::delete("/workspaces/{workspace_id}/tickets/{ticket_id}", function () {
+        return "todo";
+    });
+});
